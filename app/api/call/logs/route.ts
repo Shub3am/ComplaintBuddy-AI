a3d355c7083254e580913692339d2de068abce86
shub3am <shubhamvishwakarma0604@gmail.com>
2025-06-24 13:43:34 +0530

    fixed api route

--- app/api/call/logs/route.ts
+++ app/api/call/logs/route.ts
@@ -4,20 +4,14 @@ export async function POST(request: Request) {
   const body = await request.json();
   console.log(body);
   if (body.agent) {
-    let dispatchCall = await fetch(
+    let getLogs = await fetch(
       `https://backend.omnidim.io/api/v1/calls/logs?agentid=${body.agent}`,
       {
-        method: "POST",
         headers: { Authorization: `Bearer ${process.env.omnidim}` },
-        body: JSON.stringify({
-          agent_id: body.agent,
-          to_number: body.complain.company_phone,
-          call_context: body.complain,
-        }),
       }
     );
 
-    return NextResponse.json({ error: null });
+    return NextResponse.json({ error: null, data: getLogs });
   } else {
     return NextResponse.json({ error: "Credentials Missing" });
   }
